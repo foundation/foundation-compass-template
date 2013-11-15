72c6ca18edc42204fb465fb649ecc049a2a58b95
Mark Hayes <mark@zurb.com>
2013-11-14 17:09:27 -0800

    added semantic grid example

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ add_import_path "bower_components/foundation/scss"
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
-sass_dir = "sass"
+sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
